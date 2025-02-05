22728b7e8a5b3050b2113df5f4806bc2d28a1aa7
Harold Rajaonarison <rnarsonharold@gmail.com>
2025-02-05 14:35:10 +0100

    initial commit

---
+++ src/main/java/dev/haroldraja/movies_preview/MoviesPreviewApplication.java
@@ -0,0 +1,13 @@
+package dev.haroldraja.movies_preview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesPreviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesPreviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/haroldraja/movies_preview/MoviesPreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.haroldraja.movies_preview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesPreviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
