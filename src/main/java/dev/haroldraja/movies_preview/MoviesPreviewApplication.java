32bc2d822c94744cef3f6f4ad597224298c7d480
Harold Rajaonarison <rnarsonharold@gmail.com>
2025-02-06 15:35:27 +0100

    integrate spring doc open-api doc

--- src/main/java/dev/haroldraja/movies_preview/MoviesPreviewApplication.java
+++ src/main/java/dev/haroldraja/movies_preview/MoviesPreviewApplication.java
@@ -2,12 +2,20 @@ package dev.haroldraja.movies_preview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MoviesPreviewApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MoviesPreviewApplication.class, args);
+    }
+
+    @GetMapping("/home")
+    public String welcome(){
+        return "Welcome Home";
     }
 
 }
