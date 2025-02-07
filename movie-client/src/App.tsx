c46f720e6529ab7693f5d441d06de5c414be7889
Harold Rajaonarison <rnarsonharold@gmail.com>
2025-02-07 12:19:42 +0100

    config(client): config api url in axios and test http request

--- movie-client/src/App.tsx
+++ movie-client/src/App.tsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import api from './api/axiosConfig';
-
+import Layout from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App()
 {
@@ -13,6 +15,8 @@ function App()
     try
     {
       const response = await api.get("movies");
+
+      console.log(response.data)
 
       setMovies(response.data)
     } catch (err)
@@ -29,7 +33,12 @@ function App()
 
   return (
     <div className='App'>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route path='/' element={<Home />}></Route>
 
+        </Route>
+      </Routes>
     </div>
   )
 }
